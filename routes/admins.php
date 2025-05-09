5d3cec5d214864457cd98aaaca5e4e030f446a78
Nishad Hossain <freelancernishad123@gmail.com>
2025-05-09 22:33:32 +0600

    update

--- routes/admins.php
+++ routes/admins.php
@@ -6,10 +6,15 @@ use App\Http\Controllers\Admin\TouristPlaceController;
 use App\Http\Controllers\Admin\TouristPlaceCategoryController;
 
 
-Route::middleware(AuthenticateAdmin::class)->group(function () {
+// Route::middleware(AuthenticateAdmin::class)->group(function () {
+
+
     Route::prefix('admin')->group(function () {
         Route::apiResource('tourist-place-categories', TouristPlaceCategoryController::class);
         Route::apiResource('tourist-places', TouristPlaceController::class);
     });
-});
+
+
+
+// });
 
