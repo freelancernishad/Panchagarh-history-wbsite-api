c5f0ff2f3f2b90ef61584a1a7035dc20dfdc8af1
Nishad Hossain <freelancernishad123@gmail.com>
2025-05-14 00:13:15 +0600

    update

--- app/Http/Controllers/Public/TouristPlacePublicController.php
+++ app/Http/Controllers/Public/TouristPlacePublicController.php
@@ -11,7 +11,7 @@ class TouristPlacePublicController extends Controller
     // ✅ List All (with optional filters)
     public function index(Request $request)
     {
-        $query = TouristPlace::select('name','location','category_id','image_url')->with('category');
+        $query = TouristPlace::select('id','name','location','category_id','image_url')->with('category');
 
         if ($request->has('category_id')) {
             $query->where('category_id', $request->category_id);
